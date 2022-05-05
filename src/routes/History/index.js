aaf5f5c4bf317afbb83d40c4043e7e42b52025d7
jihun1233 <leejihun1233@gmail.com>
2022-05-05 10:55:56 +0900

    ✨History 컴포넌트 생성
    - history 화면을 담당할 컴포넌트 생성
    
    - 전체화면을 가정한 wrapper로 감싸놓음

---
+++ src/routes/History/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './History.module.scss'
+
+function History() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.centering}>
+        this is history page
+      </div>
+    </div>
+  )
+}
+
+
+export default History
