d748b29d0d77ae8ae48fdd91b0664fc16fe918b7
markyul <pig68111000@gmail.com>
2022-05-06 14:57:58 +0900

    요소 component화 삭제
    짧아서 불필요하다고 판단

--- src/routes/loginPage/index.js
+++ src/routes/loginPage/index.js
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
-import PropTypes from 'prop-types'
-
 import styles from './LoginPage.module.scss'
 
 function LoginPage() {
   const [ID, setID] = useState('')
   const [password, setPassword] = useState('')
+  const [isPasswordView, setIsPasswordView] = useState(false)
+
+  const passwordViewIcon = isPasswordView ? <FaRegEye size={15} /> : <FaRegEyeSlash size={15} />
 
   const history = useHistory()
+
+  const onChangeID = (e) => {
+    setID(e.target.value)
+  }
+
+  const onChangePW = (e) => {
+    setPassword(e.target.value)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -21,73 +30,33 @@ function LoginPage() {
     window.location.reload()
   }
 
-  return (
-    <div className={styles.loginContainer}>
-      <header>TODO LIST</header>
-      <form onSubmit={handleSubmit}>
-        <InputID ID={ID} setID={setID} />
-        <InputPW password={password} setPassword={setPassword} />
-        <LoginBtn />
-      </form>
-    </div>
-  )
-}
-
-function InputID({ ID, setID }) {
-  const onChangeID = (e) => {
-    setID(e.target.value)
-  }
-
-  return (
-    <div className={styles.inputBox}>
-      <input placeholder='ID를 입력하시오' value={ID} onChange={onChangeID} />
-    </div>
-  )
-}
-
-InputID.propTypes = {
-  ID: PropTypes.string,
-  setID: PropTypes.func,
-}
-
-function InputPW({ password, setPassword }) {
-  const [isPasswordView, setIsPasswordView] = useState(false)
-
-  const onChangePW = (e) => {
-    setPassword(e.target.value)
-  }
-
   const handlePasswordIcon = () => {
     setIsPasswordView((prev) => !prev)
   }
 
-  const passwordViewIcon = isPasswordView ? <FaRegEye size={15} /> : <FaRegEyeSlash size={15} />
-
   return (
-    <div className={styles.inputBox}>
-      <input
-        placeholder='비밀번호를 입력하시오'
-        type={!isPasswordView && 'password'}
-        value={password}
-        onChange={onChangePW}
-      />
-      <button type='button' className={styles.passwordIcon} onClick={handlePasswordIcon}>
-        {passwordViewIcon}
-      </button>
+    <div className={styles.loginContainer}>
+      <header>TODO LIST</header>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputBox}>
+          <input placeholder='ID를 입력하시오' value={ID} onChange={onChangeID} />
+        </div>
+        <div className={styles.inputBox}>
+          <input
+            placeholder='비밀번호를 입력하시오'
+            type={!isPasswordView && 'password'}
+            value={password}
+            onChange={onChangePW}
+          />
+          <button type='button' className={styles.passwordIcon} onClick={handlePasswordIcon}>
+            {passwordViewIcon}
+          </button>
+        </div>
+        <button type='submit' className={styles.loginBtn}>
+          LOGIN
+        </button>
+      </form>
     </div>
-  )
-}
-
-InputPW.propTypes = {
-  password: PropTypes.string,
-  setPassword: PropTypes.func,
-}
-
-function LoginBtn() {
-  return (
-    <button type='submit' className={styles.loginBtn}>
-      LOGIN
-    </button>
   )
 }
 
