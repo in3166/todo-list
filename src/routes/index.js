2d40f6bb985ae1b02d4758af0a48ac79b980a738
jihun1233 <leejihun1233@gmail.com>
2022-05-06 11:59:26 +0900

    🎨 Container route 바깥으로 이동
    - 모든 페이지에 적용하기 때문에 route 밖에서 감싸줌

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,20 @@ import MainPage from './mainPage'
 import LoginPage from './loginPage'
 import HistoryPage from './historyPage'
 import SettingPage from './settingPage'
+import Container from '../components/Container'
 
 function App() {
   return (
     <Router>
       <div className={styles.app}>
-        <Switch>
-          <Route exact path='/' component={MainPage} />
-          <Route exact path='/login' component={LoginPage} />
-          <Route exact path='/history' component={HistoryPage} />
-          <Route exact path='/setting' component={SettingPage} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path='/' component={MainPage} />
+            <Route exact path='/login' component={LoginPage} />
+            <Route exact path='/history' component={HistoryPage} />
+            <Route exact path='/setting' component={SettingPage} />
+          </Switch>
+        </Container>
       </div>
     </Router>
   )
