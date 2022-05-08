d6d92aaf5d58e25284d0fc19dc2d59a34eddb42b
김학률 <52916848+markyul@users.noreply.github.com>
2022-05-08 14:07:20 +0900

    Refac: 1차 Refactoring
    #26

--- src/routes/mainPage/todoList/Todo.js
+++ src/routes/mainPage/todoList/Todo.js
@@ -1,7 +1,7 @@
 import styles from './Todo.module.scss'
 import PropTypes from 'prop-types'
 import { cx } from '../../../styles'
-import { FaCheckCircle } from 'react-icons/fa'
+// import { FaCheckCircle } from 'react-icons/fa'
 import { memo } from 'react'
 
 function Todo({ id, task, category, completed, onClick }) {
@@ -13,19 +13,19 @@ function Todo({ id, task, category, completed, onClick }) {
     <li className={styles.todoContainer}>
       <div className={styles.checkBox}>
         <button
-          className={cx(styles.checkBtn, styles[category])}
+          className={cx(styles.checkBtn, styles[category], { [styles[`${category}Selected`]]: completed })}
           aria-label='Save'
           type='button'
           onClick={handleClick}
         />
-        {completed && <FaCheckCircle onClick={handleClick} />}
+        {/* {completed && <FaCheckCircle onClick={handleClick} />} */}
       </div>
       {/* <div className={cx(styles.taskMessage, { [styles.taskMessageCompleted]: completed })}>{task}</div> */}
       <div className={styles.taskMessageBox}>
-        <p className={styles.taskMessage}>
+        <div className={styles.taskMessage}>
           {task}
           {completed && <div className={styles.taskMessageLine} />}
-        </p>
+        </div>
       </div>
     </li>
   )
